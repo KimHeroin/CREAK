b60d7bc179d2b08476281ca4325cde6e146c52c1
KimHeroin <yebboona@snu.ac.kr>
2022-12-05 02:55:33 +0900

    gochu

---
+++ dist/script.js
@@ -0,0 +1,30 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Models
+ */
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+let mixer = null
+
+gltfLoader.load(
+    '/models/cyborg.glb',
+    (gltf) =>
+    {
+        scene.add(gltf.scene)
+    }
+)
